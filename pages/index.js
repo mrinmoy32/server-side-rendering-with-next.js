580009dee07f6bfe4c93b0ace7674524757997f4
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-12-10 23:03:21 +0530

    Added data source

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
-export default () => (
-    <p>Hello</p>
-)
+import React from 'react'
+import styles from './index.module.css'
+import Card from './Card'
+import data from './API/data.json'
+
+export default class Index extends React.Component {
+
+    static async getInitialProps() {
+        return { card: data }
+    }
+
+    render() {
+        return (
+            <div className={styles.app}>
+                <header className={styles.header}>
+                    <img src='logo.png' className={styles.logo} />
+                </header>
+                <div className={styles.grid}>
+                    {this.props.cards.map(card => <Card key={card.id} />)}
+                </div>
+            </div>
+        )
+    }
+}
