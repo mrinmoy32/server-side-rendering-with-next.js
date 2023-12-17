68dcc830864a2c5dba7a3b4cfd30f08ac9f1e09e
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-12-17 23:37:04 +0530

    creating 2 func returning HTLM, as pages

---
+++ pages/ohyeah.js
@@ -0,0 +1,5 @@
+const ohyeah = () => (
+    <h1>I'm page 3 trapped inside of OH YEAH!</h1>
+)
+
+export default ohyeah
---
+++ pages/page2.js
@@ -0,0 +1,5 @@
+const page2 = () => (
+    <h1>I'm page 2</h1>
+)
+
+export default page2
