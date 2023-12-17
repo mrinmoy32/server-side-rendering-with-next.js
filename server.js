2ad059b32cd3e74a3ecc6b0f07de3c2d3bd5ee62
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-12-17 23:37:22 +0530

    handing routes

--- server.js
+++ server.js
@@ -3,14 +3,25 @@ const next = require('next');
 
 const port = 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({dev});
-const handle = app.getRequestHandle();
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare()
-.then(() => {
-    const server = express();
-    server.listen(port, (err) => {
-        if(err) throw err;
-        console.log(`Ready on http://localhost:${port}`)
+    .then(() => {
+        const server = express();
+
+        server.get('/page2', (req, res) => {
+            return app.render(req, res, '/page2');
+        })
+        server.get('/page3', (req, res) => {
+            return app.render(req, res, '/ohyeah');
+        })
+        server.get('*', (req, res) => {
+            return handle(req, res);
+        })
+
+        server.listen(port, (err) => {
+            if (err) throw err;
+            console.log(`Ready on http://localhost:${port}`)
+        })
     })
-})
